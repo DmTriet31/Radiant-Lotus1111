adc86ed216131b772c2370b26d1b02353c0d8be9
DmTriet31 <dmtriet.31@gmail.com>
2025-05-07 12:55:27 +0200

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,11 +1,10 @@
-
 module.exports = {
     enabled: true, 
     lavalink: {
-        identifier: 'GlaceYT',
-        password: 'glaceyt',
-        host: '193.226.78.187',
-        port: 9372,
+        identifier: 'DmTriet',
+        password: 'Radiant Lotus',
+        host: '78.46.65.243',
+        port: 4644,
         secure: false
     }
 };
