ca1714fc863f6799e2b87ed1f9300d7c7873d310
DmTriet31 <dmtriet.31@gmail.com>
2025-05-08 14:53:07 +0200

    Update start-server.js

--- commands/utility/start-server.js
+++ commands/utility/start-server.js
@@ -1,4 +1,3 @@
-// commands/start-server.js
 const puppeteer = require('puppeteer');
 
 const USERNAME = 'MinhTriet06';
@@ -11,17 +10,18 @@ module.exports = {
     try {
       await message.reply('Đang khởi động server Aternos...');
 
-      const browser = await puppeteer.launch({ headless: true });
+      const browser = await puppeteer.launch({ headless: true });  // Bạn có thể đổi thành false khi muốn xem
       const page = await browser.newPage();
 
       await page.goto('https://aternos.org/go/');
+      await page.waitForSelector('#user');  // Đảm bảo form đăng nhập đã tải
       await page.type('#user', USERNAME);
       await page.type('#password', PASSWORD);
       await page.click('#login');
       await page.waitForNavigation();
 
       await page.goto('https://aternos.org/server/');
-      await page.waitForSelector('#start');
+      await page.waitForSelector('#start');  // Đảm bảo nút start đã sẵn sàng
       await page.click('#start');
 
       await message.channel.send('Đã gửi yêu cầu bật server!');
